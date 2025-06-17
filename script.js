472cca11a9a89adb62cff277e81c8ab4c68e2721
Mai Thuy Hong <thuyhongmai2004@gmail.com>
2025-06-17 13:57:20 +0700

    Thay đổi để thêm nhánh xem chi từng từng phim

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 const movies = [
   {
+    id: "1",
     title: "Doraemon: Nobita và Vũ trụ phiêu lưu ký",
     image: "https://thuviensach.vn/img/news/2024/06/larger/16170-doraemon-movie-nobita-va-vu-tru-phieu-luu-ky-1.jpg?v=3265",
     trailer: "https://www.youtube.com/watch?v=RMv6yKBR7hU"
   },
   {
+    id: "2",
     title: "Doraemon: Nobita và Đảo giấu vàng",
     image: "https://i.ytimg.com/vi/98IizofPo7s/maxresdefault.jpg",
     trailer: "https://www.youtube.com/watch?v=RtKGs7IQQD8"
-  },
-  {
-    title: "Doraemon: Nobita và Hiệp sĩ rồng",
-    image: "https://newshop.vn/public/uploads/products/30636/sach-doraemon-tranh-truyen-mau-tap-9-nobita-va-hiep-si-rong.jpg",
-    trailer: "https://www.youtube.com/watch?v=khn7zECoZJY"
-  },
-  {
-    title: "Doraemon: Nobita và Mặt trăng phiêu lưu ký",
-    image: "https://tse4.mm.bing.net/th?id=OIP.Qjj5CFg6Q_j0iAVrqmglaAAAAA&pid=Api&P=0&h=180",
-    trailer: "https://www.youtube.com/watch?v=G5Hg5h9eEc4"
-  },
-  {
-    title: "Doraemon: Nobita và Xứ sở nghìn lẻ một đêm",
-    image: "https://tse2.mm.bing.net/th?id=OIP.YJE_MsvoT5POKSStEPg3rAHaLm&pid=Api&P=0&h=180",
-    trailer: "https://www.youtube.com/watch?v=dnv-4-VY6Ks"
   }
+  // Thêm phim khác nếu muốn
 ];
 
 const listDiv = document.getElementById("movie-list");
@@ -31,12 +19,11 @@ const listDiv = document.getElementById("movie-list");
 movies.forEach(movie => {
   const movieDiv = document.createElement("div");
   movieDiv.className = "movie";
-
   movieDiv.innerHTML = `
     <img src="${movie.image}" alt="${movie.title}">
     <h2>${movie.title}</h2>
-    <button onclick="window.open('${movie.trailer}', '_blank')">Xem trailer</button>
+    <button onclick="window.open('${movie.trailer}', '_blank')">Xem trailer</button><br>
+    <a href="movie_1.html?id=${movie.id}" class="back-button">Xem chi tiết</a>
   `;
-
   listDiv.appendChild(movieDiv);
 });
